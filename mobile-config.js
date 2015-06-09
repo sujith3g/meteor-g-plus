03e950eb7f22cdda20539a0047b3b9060897b6d4
Sujith G <sujith3g@gmail.com>
2015-06-10 00:44:38 +0530

    initial commit

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.example.meteor.gplus',
+  name: 'meteor-g-plus',
+  description: 'Metor Google Plus Login Example',
+  author: 'Sujith G',
+  email: 'sujith3g@gmail.com',
+  website: 'http://github.com/sujith3g'
+});
