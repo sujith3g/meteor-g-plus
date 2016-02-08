d7c9219b057f33e32d426407b26740fbbb45b358
Sujith G <sujith3g@gmail.com>
2016-02-09 00:29:01 +0530

    Update config.accounts.js

--- server/config.accounts.js
+++ server/config.accounts.js
@@ -1,4 +1,5 @@
 var google = {
+    // Use OAuth Credentials of web application/client here, for oauth from web browser.
     clientId: "81242494817-38ohse21enq5ibpe65155qp82lr6h7e7.apps.googleusercontent.com",
     clientSecret: "SXV_5gjOLtwhoCnYE_2oh9Et"
 };
