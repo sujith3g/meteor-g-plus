ede72f6a8e586ccc00066e0d6691cd8daf916444
Sujith G <sujith3g@gmail.com>
2016-12-18 12:53:38 +0530

    Update hedcet:cordova-google-plus-native-sign-in

--- client/meteor-g-plus.js
+++ client/meteor-g-plus.js
@@ -17,7 +17,8 @@ Template.googleSignIn.events({
         if (Meteor.isCordova) { // signIn through cordova
             Meteor.cordova_g_plus({
                 cordova_g_plus: true,
-                profile: ["email", "email_verified", "gender", "locale", "name", "picture"] // customized Meteor.user() pfofile ["email", "email_verified", "family_name", "gender", "given_name", "locale", "name", "picture", "profile", "sub"]
+                profile: ["email", "email_verified", "family_name", "gender", "given_name", "locale", "picture"], // customized Meteor.user() pfofile ["email", "email_verified", "family_name", "gender", "given_name", "locale", "name", "picture", "profile", "sub"]
+                webClientId: '767912667782-bonpfuqdmu5jmn1bp1a8bgg6mdcorgc5.apps.googleusercontent.com'
             }, function(error) {
                 if (error) alert(error);
                 // else location.reload();
